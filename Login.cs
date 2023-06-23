a9d1751f92b825afdfa5104678ab43440ac22648
arshiyairfan <arshiyairfan04@gmail.com>
2023-06-23 10:21:35 +0500

    .

--- Login.cs
+++ Login.cs
@@ -1,18 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-using Microsoft.WindowsAPICodePack.Shell;
-using Microsoft.WindowsAPICodePack.Shell.Interop;
-
-using System.Runtime.InteropServices;
 
 
 
